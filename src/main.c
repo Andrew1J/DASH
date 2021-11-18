9ac41ade4290374fe657756fac892edd69a3a060
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-11-18 13:54:02 -0500

    started main loop

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "parse.h"
 #include "run.h"
 
 int main () {
+	char input[100]; // TODO: make it expand to the input size
+	char cwd[4096];  // see https://www.google.com/search?q=unix+path+length
+
+	while (1) {
+		getcwd(cwd, 4096);
+		printf("%s$ ", cwd);
+	
+		fgets(input, sizeof(input), stdin);
+		input[strlen(input) - 1] = '\0';  // remove trailing newline
+
+		if (strcmp(input, "exit") == 0) break;
+		
+		printf("%s\n", input);  // just echo it for now
+	}
+
     return 0;
 }
