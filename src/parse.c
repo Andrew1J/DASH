6ec6aec5cb7e618efeb91454a7eb02a7a80641f0
Dylan Hu <43192121+dylanhhu@users.noreply.github.com>
2021-11-19 09:24:12 -0500

    started read_line

--- src/parse.c
+++ src/parse.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 #include "parse.h"
+
+// TODO: finish
+// Reads in a line from stdin and does
+// related processing to clean it up
+// 
+// Returns a pointer to the line
+char * read_line() {
+    char *input = malloc(100 * sizeof(char));
+
+    fgets(input, sizeof(input), stdin);
+
+    return input;
+}
--- src/parse.h
+++ src/parse.h
@@ -1,6 +1,6 @@
 #ifndef PARSE_H
 #define PARSE_H
 
-
+char * read_line();
 
 #endif
