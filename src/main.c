c615b6d2a5a6700f70fae48e61c7a49064288ad5
Andrew <andrewjuang01@gmail.com>
2021-11-24 09:43:11 -0500

    confusion

--- src/main.c
+++ src/main.c
@@ -11,40 +11,40 @@ void tests();
 
 int main(int argc, char *argv[]) {
 	// tests();
-
+	
 	char cwd[4096];  // see https://www.google.com/search?q=unix+path+length
 
 	while (1) {
-        // Print prompt
+		// Print prompt
 		print_prompt();
 
-    	// Read in line from STDIN
-    	char *input = read_line();
+		// Read in line from STDIN
+		char *input = read_line();
 
-        // Parse the commands from input
-        // TODO: will have to trim whitespace later
-        char **commands = parse_args(input, ';');
+		// Parse the commands from input
+		// TODO: will have to trim whitespace later
+		char **commands = parse_args(input, ';');
 
-        // Execute commands
-        int i = 0;
-        while (commands[i]) {
-            char **args = parse_args(commands[i], ' ');
+		// Execute commands
+		int i = 0;
+		while (commands[i]) {
+			char **args = parse_args(commands[i], ' ');
 
-            if (is_shell_cmd(args)) {
-                do_shell_cmd(args);
-            } else {
-                run_command(args);
-            }
+			if (is_shell_cmd(args)) {
+				do_shell_cmd(args);
+			} else {
+				run_command(args);
+			}
 
 			free(args);
-            i++;
-        }
+			i++;
+		}
 
 		free(input);
 		free(commands);
 	}
-	
-    return 0;
+
+	return 0;
 }
 
 
@@ -52,5 +52,5 @@ void tests() {
 	command_tests();
 	run_tests();
 
-    exit(0);
+	exit(0);
 }
